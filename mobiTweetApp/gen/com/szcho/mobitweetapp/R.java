d568bff3fcd9a69dce006114e5a94087042ec5d3
priitv <priitv@gmail.com>
2012-10-14 05:06:23 +0300

    brought strings to separate fail

--- mobiTweetApp/gen/com/szcho/mobitweetapp/R.java
+++ mobiTweetApp/gen/com/szcho/mobitweetapp/R.java
@@ -15,34 +15,36 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int button_send_tweet=0x7f070006;
-        public static final int created_at=0x7f070002;
-        public static final int edittext_search=0x7f070008;
-        public static final int edittext_send_tweet=0x7f070005;
-        public static final int hello=0x7f070001;
-        public static final int home=0x7f070009;
-        public static final int layoutForScroll=0x7f070000;
-        public static final int list=0x7f07000b;
-        public static final int log_out=0x7f07000c;
-        public static final int search=0x7f070007;
-        public static final int text=0x7f070004;
-        public static final int tweet=0x7f07000a;
-        public static final int user_name=0x7f070003;
+        public static final int button_send_tweet=0x7f070004;
+        public static final int created_at=0x7f070000;
+        public static final int edittext_search=0x7f070006;
+        public static final int edittext_send_tweet=0x7f070003;
+        public static final int home=0x7f070007;
+        public static final int list=0x7f070009;
+        public static final int log_out=0x7f07000a;
+        public static final int search=0x7f070005;
+        public static final int text=0x7f070002;
+        public static final int tweet=0x7f070008;
+        public static final int user_name=0x7f070001;
     }
     public static final class layout {
-        public static final int activity_log_in=0x7f030000;
-        public static final int listrow=0x7f030001;
-        public static final int send_tweet=0x7f030002;
-        public static final int timeline=0x7f030003;
+        public static final int listrow=0x7f030000;
+        public static final int send_tweet=0x7f030001;
+        public static final int timeline=0x7f030002;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello=0x7f040001;
-        public static final int menu_log_out=0x7f040002;
-        public static final int title_activity_log_in=0x7f040003;
+        public static final int button_home=0x7f040004;
+        public static final int button_search=0x7f040003;
+        public static final int button_send=0x7f040006;
+        public static final int button_tweet=0x7f040005;
+        public static final int label_example=0x7f040008;
+        public static final int label_send_tweet=0x7f040007;
+        public static final int menu_log_out=0x7f040001;
+        public static final int title_activity_log_in=0x7f040002;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
