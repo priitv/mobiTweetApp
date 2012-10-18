2fa9f1d5e3d5efe9d2d49634bd3a22246f2d6b4f
PriitV <PriitV@priit>
2012-10-19 02:54:48 +0300

    added left characters counter to tweet sending

--- mobiTweetApp/src/com/szcho/mobitweetapp/TweetActivity.java
+++ mobiTweetApp/src/com/szcho/mobitweetapp/TweetActivity.java
@@ -3,11 +3,14 @@ package com.szcho.mobitweetapp;
 import com.szcho.mobitweetapp.networkRequestTasks.SendTweetTask;
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Activity to post tweets.. Nothing fancy
@@ -19,6 +22,7 @@ public class TweetActivity extends Activity {
 	private TwitterData twitter;
 	private Button send;
 	private EditText text;
+	private TextView label;
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,6 +33,9 @@ public class TweetActivity extends Activity {
     	twitter = new TwitterData(this);
     	send = (Button) findViewById(R.id.button_send_tweet);
     	text = (EditText) findViewById(R.id.edittext_send_tweet);
+    	label = (TextView) findViewById(R.id.label_send_tweet);
+    	
+    	text.addTextChangedListener(mTextEditorWatcher);
     	
     	send.setOnClickListener(new OnClickListener() {
 			public void onClick(View arg0) {
@@ -37,4 +44,18 @@ public class TweetActivity extends Activity {
 			}
 		});
 	}
+	
+	private final TextWatcher mTextEditorWatcher = new TextWatcher() {
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+        	label.setText("Send Tweet(" + (140-text.length()) + "):");
+        }
+
+		public void afterTextChanged(Editable arg0) {
+			
+		}
+};
+	
 }
